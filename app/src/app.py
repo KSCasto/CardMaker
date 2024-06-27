896ad94a1c9578d551dd361767fa4471c3b56cef
KSCasto <kcasto68@gmail.com>
2024-06-27 16:56:31 -0600

    Fixed Authentication placeholder, need OAuth 2.0 or similar later

--- app/src/app.py
+++ app/src/app.py
@@ -1,10 +1,10 @@
 import boto3, json, os
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from services.blueprints.users.users_blueprint import app as usersApp
 from services.blueprints.cardMaker.cm_blueprint import CardMaker as cmBp
 from services.utils.auth import authenticate
-# from dotenv import dotenv_values as env
 from dotenv import load_dotenv
+import logging
 
 
 app = Flask(__name__)
@@ -20,11 +20,21 @@ app.config['CM_OUTPUT_FOLDER'] = '/app/services/blueprints/cardMaker/Output'
 app.register_blueprint(usersApp)
 app.register_blueprint(cmBp)
 
-# @app.before_request
-# def pre_call():
-#     token = request.headers.get('Authorization')
-#     email = request.json.get('Email')
-#     authenticate(email,token)
+@app.before_request
+def pre_call():
+    token = request.headers.get('Authorization')
+    uid = request.headers.get('UID')
+
+    if token and token.startswith("Bearer "):
+        token = token[len("Bearer "):]
+
+    logging.info(f"Authenticating with {token},{uid}...")
+    authenticated=authenticate(uid,token)
+    logging.info(f"Authenticated")
+
+    if not authenticated:
+        logging.warning("Unauthorized access attempt")
+        return jsonify({'error': 'Unauthorized access'}), 401
 
 @app.route("/healthcheck")
 def healthCheck():
--- app/src/services/utils/auth.py
+++ app/src/services/utils/auth.py
@@ -2,11 +2,11 @@ import boto3
 import pyotp, os, base64
 from .db import connect
 
-def authenticate(email,pw):
+def authenticate(uid,token):
     dynamodb = connect()
     table=dynamodb.Table('user_auth')
     res=table.get_item(Key={
-        'email':email,
-        'pw':pw
+        'user_id':uid,
+        'token':token
     })
     return ('Item' in res)
